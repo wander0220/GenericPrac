74457fbb6bec35647d686c6ad95e96796b4b5b97
wander0220 <s2018s26@e-mirim.hs.kr>
2020-06-26 12:05:52 +0900

    코드보다가 <>가 보인다. 제너릭. T는 타입이지 않을까?

--- GenericPrac/Program.cs
+++ GenericPrac/Program.cs
@@ -2,14 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GenericPrac
 {
     class Program
     {
+        class Wanted<T>
+        {
+            public T value;
+
+            public Wanted(T value)
+            {
+                this.value = value;
+            }
+        }
+
         static void Main(string[] args)
         {
+            Wanted<string> wantedString = new Wanted<string>("String");
+            Wanted<int> wantedInt = new Wanted<int>(920116);
+            Wanted<double> wantedDouble = new Wanted<double>(52.273);
+
+
+            Console.WriteLine(wantedString.value);
+            Console.WriteLine(wantedInt.value);
+            Console.WriteLine(wantedDouble.value);
+
+
         }
+
+
     }
 }
